94792b119d68ff92ea8316be31ab8f3fd4875101
davidgeorgebell <bell.dave93@gmail.com>
2021-05-30 16:59:05 +0100

    Pokedex component

--- components/Pokedex.tsx
+++ components/Pokedex.tsx
@@ -1,17 +1,28 @@
 import Image from 'next/image';
+import Link from 'next/link';
 export const Pokedex = ({ pokemons }) => {
   return (
     <div className='pokedex'>
       {pokemons.map(pokemon => (
-        <div className='pokemon'>
-          <h3>{pokemon.name}</h3>
-          <Image
-            src={pokemon.image}
-            alt={pokemon.name}
-            width={300}
-            height={400}
-          />
-        </div>
+        <Link href='/' passHref>
+          <a>
+            <div className='pokemon'>
+              <h3>{pokemon.name}</h3>
+              <p>{pokemon.number}</p>
+              <Image
+                src={pokemon.image}
+                alt={pokemon.name}
+                width={200}
+                height={200}
+              />
+              <ul>
+                {pokemon.types.map(type => (
+                  <li>{type}</li>
+                ))}
+              </ul>
+            </div>
+          </a>
+        </Link>
       ))}
     </div>
   );
